b318bdcb78fd7b40b0eb579a9f6ab862dc4a8275
Sonomi Modica <sonom.jp.2013@gmail.com>
2022-01-20 23:52:17 -0500

    Learning CPP 01

--- Day2.cpp/helloworld.cpp
+++ Day2.cpp/helloworld.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <iostream> //header file library, (almost) always appear in program
 
-int main()
+int main() //function
 {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello World!";
+    return 0;
+    //std:: - use names for obj & var from standard (std) library
+    //count(see-out): object to print text
+    //insertion operator: <<
+    //end statements in ; like Java
+    //return 0: ends main function
 }
