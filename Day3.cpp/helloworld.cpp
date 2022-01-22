61a74e4691a47d0272002cea448085fad6e70e45
Sonomi Modica <sonom.jp.2013@gmail.com>
2022-01-21 21:16:51 -0500

    Hello World cont.

---
+++ Day3.cpp/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //header file library, (almost) always appear in program
+
+int main() //function
+{
+    std::cout << "Hello World! \n";
+    std::cout << "I am learning C++";
+    return 0;
+    //std:: - use names for obj & var from standard (std) library
+    //count(see-out): object to print text
+    //insertion operator: <<
+    /*use \n to create blank line between txt
+    or use >> end1 */
+    //end statements in ; like Java
+    //return 0: ends main function
+}
