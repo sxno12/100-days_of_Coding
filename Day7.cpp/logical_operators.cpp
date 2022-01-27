f0a0b3f29746cc821b243a2621c7a4c5cfe3f967
SM <90343927+sxno12@users.noreply.github.com>
2022-01-26 21:01:29 -0500

    Operators in CPP

--- Day7.cpp/logical_operators.cpp
+++ Day7.cpp/logical_operators.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+//determine logic between variables/values
+
+int x = 8;
+int y = 2;
+float a, b, c;
+
+int main(){
+    a = x < 5 && x < 10;     //logical and: if both true, than true
+    b = y < 5 || y < 4;      //logical or: if one is true, than true
+    c = !(x < 5 && x < 10);  //logical not: if false, than true and vice versa
+
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << "\n";
+}
