713e59e4c68ef45072eef8a5e99a2191f57c36af
SM <90343927+sxno12@users.noreply.github.com>
2022-01-26 20:53:20 -0500

    Add files via upload

--- Day7.cpp/comparison_operators.cpp
+++ Day7.cpp/comparison_operators.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+//return value of comparison: true(1) or false (0)
+
+int x = 5;
+int y = 3;
+
+int main(){
+    //cout << (x > y);
+    cout << (x == y);
+}
+
+/* == : equal to
+   != : not equal
+   >  : greater than
+   <  : less than
+   >= : greater than or equal to
+   <= : less than or equal to */
