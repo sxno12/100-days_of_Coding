c1633ca4f9757856de1554a28f23dd118a9d4cdc
SM <90343927+sxno12@users.noreply.github.com>
2022-01-28 00:20:41 -0500

    Math & Booleans

--- Day8.cpp/Boolean/boolean_expressions.cpp
+++ Day8.cpp/Boolean/boolean_expressions.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int x = 10;
+int y = 9;
+
+int main(){
+    cout << (x > y) << "\n";
+    cout << (x == 10) << "\n";
+    cout << (10 == 15);
+}
--- Day8.cpp/Boolean/boolean_values.cpp
+++ Day8.cpp/Boolean/boolean_values.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+bool isCodingFun = true;
+bool isFishTasty = false;
+
+int main(){
+    cout << isCodingFun << "\n";
+    cout << isFishTasty;
+}
