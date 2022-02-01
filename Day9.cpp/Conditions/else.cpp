e59ea88fd286653f953312c71e218259f492bbf1
SM <90343927+sxno12@users.noreply.github.com>
2022-02-01 00:20:20 -0500

    Strings & Switch

---
+++ Day9.cpp/Conditions/else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//use 'else' to specify a block of code when false
+
+int main() {
+    int time = 20;
+     if(time < 18) {
+         cout << "Good day.";
+     } else {
+         cout << "Good evening.";
+     }
+
+     return 0;
+ }
---
+++ Day9.cpp/Conditions/if.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//use 'if' to specify a block of code to be executed when true
+
+int x = 20;
+int y = 18;
+
+int main(){
+    if (x > y) {
+        cout << "x is greater than y";
+    }
+}
